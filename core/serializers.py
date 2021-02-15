d215d40321d5dc62ace4e4d51854be93806e6782
Talha Jubair <talhajubair100.bd@gmail.com>
2021-02-15 19:58:46 +0600

    nested relation added

--- core/serializers.py
+++ core/serializers.py
@@ -3,11 +3,27 @@ from django.db.models import fields
 from rest_framework import serializers
 from .models import Customer, Profession, DataSheet, Document
 
+
+class DataSheetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataSheet
+        fields = ('description', 'histoy_data')
+
+
+class ProfessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profession
+        fields = ('description', )
+
+
+
 class CustomerSerializer(serializers.ModelSerializer):
     number_profession = serializers.SerializerMethodField()
     #data_sheet = serializers.StringRelatedField()
-    data_sheet = serializers.SerializerMethodField()
-    profession = serializers.StringRelatedField(many=True)
+    #data_sheet = serializers.SerializerMethodField()  
+    data_sheet = DataSheetSerializer() #for nested relationship
+    #profession = serializers.StringRelatedField(many=True)
+    profession = ProfessionSerializer(many=True)  #for nested relationship
     document_set = serializers.StringRelatedField(many=True)
 
     class Meta:
@@ -20,17 +36,21 @@ class CustomerSerializer(serializers.ModelSerializer):
     def get_data_sheet(self, obj):
         return obj.data_sheet.description
 
-class ProfessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profession
-        fields = ('description')
+# class ProfessionSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Profession
+#         fields = ('description')
 
-class DataSheetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DataSheet
-        fields = ('description', 'histoy_data')
+
 
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = ('doc_type', 'doc_name', 'customer')
+
+
+# class DataSheetSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = DataSheet
+#         fields = ('description', 'histoy_data')
+        
